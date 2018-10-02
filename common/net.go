d9c0400ce234d74dfebc08d48db8b386a40171c0
Sacha Kozma <sacha.kozma@gmail.com>
2018-10-02 09:51:46 +0200

    Added new messages for step 2

--- common/net.go
+++ common/net.go
@@ -7,12 +7,29 @@ import (
 )
 
 type SimpleMessage struct {
-	OriginalName  string
-	Contents      string
+	OriginalName string
+	Contents     string
+}
+
+type RumorMessage struct {
+	Origin string
+	ID     uint32
+	Text   string
+}
+
+type PeerStatus struct {
+	Identifier string
+	NextID     uint32
+}
+
+type StatusPacket struct {
+	Want []PeerStatus
 }
 
 type GossipPacket struct {
 	Simple *SimpleMessage
+	Rumor  *RumorMessage
+	Status *StatusPacket
 }
 
 func SendMessage(address string, packet *GossipPacket, conn *net.UDPConn) error {
