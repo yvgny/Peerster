d3a5b7f5585402d101d8d675c8ca50c206bf3818
Sacha Kozma <sacha.kozma@gmail.com>
2018-11-05 18:51:48 +0100

    Files metadata are now saved in internal data structures

--- DataManager.go
+++ DataManager.go
@@ -14,7 +14,12 @@ import (
 const DataCacheFolder = ".peerster"
 
 type DataManager struct {
-	data sync.Map
+	records sync.Map
+}
+
+type FileMetaData struct {
+	name string
+	size int64
 }
 
 func NewDataManager() *DataManager {
@@ -22,7 +27,7 @@ func NewDataManager() *DataManager {
 		os.Mkdir(DataCacheFolder, os.ModePerm)
 	}
 	return &DataManager{
-		data: sync.Map{},
+		records: sync.Map{},
 	}
 }
 
@@ -67,6 +72,13 @@ func (dm *DataManager) addFile(path string) ([]byte, error) {
 		return nil, err
 	}
 
+	fs, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	dm.addRecord(filename, file.Name(), fs.Size())
+
 	return metafileHash[:], nil
 }
 
@@ -85,6 +97,25 @@ func (dm *DataManager) addData(data, hash []byte) error {
 	}
 
 	return nil
+}
+
+func (dm *DataManager) addRecord(hash, filename string, size int64) {
+	md := FileMetaData{
+		name: filename,
+		size: size,
+	}
+
+	dm.records.Store(hash, md)
+}
+
+func (dm *DataManager) getRecord(hash string) (*FileMetaData, error) {
+	rawData, ok := dm.records.Load(hash)
+	if !ok {
+		return nil, errors.New("cannot find requested record")
+	}
+	md := rawData.(FileMetaData)
+
+	return &md, nil
 }
 
 func (dm *DataManager) getData(hash []byte) ([]byte, error) {
