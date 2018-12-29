476ea30f6cac63f6732e3dfc06aee6a2bb4203a4
Theyiot <pprizee@gmail.com>
2018-12-29 17:08:54 +0100

    A destination field has been added in UploadedFileReply for consistance

--- common/net.go
+++ common/net.go
@@ -171,9 +171,10 @@ type FileUploadAck struct {
 }
 
 type UploadedFileRequest struct {
-	Origin	 string
-	MetaHash [32]byte
-	Nonce    [32]byte
+	Origin		string
+	Destination	string
+	MetaHash 	[32]byte
+	Nonce    	[32]byte
 }
 
 type UploadedFileReply struct {
